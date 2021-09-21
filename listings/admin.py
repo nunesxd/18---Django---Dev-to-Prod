20db26426b07b15fdab060acbcdef63bb0a99fca
Rafael Nunes <Rafael.Nunes@br.ey.com>
2021-09-21 19:07:34 -0300

    Adição de customização das tabelas na sessão de admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Listing
 
+
+class ListingAdmin(admin.ModelAdmin):
+    # O que estiver em parênteses irá aparecer como coluna em nossa tabela no admin:
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    # Este atributo permitirá entrar no elemento clicando nele, por padrão apenas o primeiro elemento permite isto, no caso o 'id' definido acima.
+    list_display_links = ('id', 'title')
+    # Funcionalidade filtragem as colunas selecionadas:
+    list_filter = ('realtor',)
+    # Funcionalidade de edição das colunas selecionadas:
+    list_editable = ('is_published',)
+    # Funcionalidade de procurar, search, nas colunas selecionadas:
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode', 'price')
+    # Funcionalidade de paginação:
+    list_per_page = 25
+
+
 # Estamos adicionando o modelo de 'Listing' ao nossos admins, de forma que eles podem manipulá-los.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
