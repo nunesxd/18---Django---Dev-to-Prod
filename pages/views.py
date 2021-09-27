3776c95c724c7f6c7c1c3b4a484e8b352f9ea893
Rafael Nunes <Rafael.Nunes@br.ey.com>
2021-09-27 17:38:30 -0300

    Alteração do template do home e do about

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Mesmo do módulo 'pages' podemos importar o ORM de 'listings' e usá-lo:
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
     # Um retorno que será usado apenas para o teste da criação de nossa rota:
     # return HttpResponse('<h1>Hello World</h1>')
-    return render(request, 'pages/index.html')
+
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+
+    context = {
+        "listings": listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Pega todos os vendedores:
+    realtors = Realtor.objects.order_by('-hire_date')
+    # Pega apenas o MVP:
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        "realtors": realtors,
+        "mvp_realtors": mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
