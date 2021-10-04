577650e5e87a38383b7090a770d8b7355994127a
Rafael Nunes <Rafael.Nunes@br.ey.com>
2021-10-04 15:06:42 -0300

    Adição do login, da autenticação

--- accounts/views.py
+++ accounts/views.py
@@ -44,14 +44,18 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Testando os alertas de erro de nosso site:
-        messages.error(request, 'Testing error message')
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
 
-        context = {
+        user = auth.authenticate(username=username, password=password)
 
-        }
-
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
