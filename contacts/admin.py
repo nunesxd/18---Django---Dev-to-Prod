a851431a10049794cab0a3ca85fa2cb0a2747e74
Rafael Nunes <Rafael.Nunes@br.ey.com>
2021-10-06 11:03:40 -0300

    Alteração do template 'listing', quanto ao modal 'inquiry', adições no app contact

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing',
+                    'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+
+# Estamos adicionando o modelo de 'Listing' ao nossos admins, de forma que eles podem manipulá-los.
+admin.site.register(Contact, ContactAdmin)
